8720f7141c7b322951cc10fa731ff0971f46061a
Nabila Sarvari <63661686+NSarvari@users.noreply.github.com>
2021-04-26 00:46:39 +0300

    Exercises- If-Else

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace HotelRoom
+{
+    class Program
+    {
+        static void Main()
+        {
+            string month = Console.ReadLine();
+            int night = int.Parse(Console.ReadLine());
+
+            double priceOfStudio = 0;
+            double priceOfApartment = 0;
+
+            if (month == "May" || month == "October")
+            {
+                priceOfApartment = 65;
+
+                if (night > 7 && night <= 14)
+                {
+                    double price = 50 * 0.05;
+                    priceOfStudio = 50-price;
+                }
+                else if (night > 14)
+                {
+                    double price1 = 50 * 0.3;
+                    priceOfStudio = 50 - price1;
+                    double price2 = 65 * 0.1;
+                    priceOfApartment =  65- price2;
+                }
+            }
+
+            else if (month == "June" || month == "September")
+            {
+                priceOfStudio = 75.20;
+                priceOfApartment = 68.7;
+
+                if (night > 14)
+                {
+                    double price1 = 75.20 * 0.2;
+                    priceOfStudio = 75.20- price1;
+                    double price2 = 68.7 * 0.1;
+                    priceOfApartment =  68.7- price2;
+                }
+            }
+            else if (month == "July" || month == "August")
+            {
+                priceOfStudio = 76;
+                priceOfApartment = 77;
+                if (night > 14)
+                {
+                    double price = 77 * 0.1;
+                    priceOfApartment =  77- price;
+                }
+            }
+            Console.WriteLine($"Apartment: {(priceOfApartment*night):f2} lv.");
+            Console.WriteLine($"Studio: {(priceOfStudio*night):f2} lv.");
+        }
+    }
+}
